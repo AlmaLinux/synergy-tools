6c5da8beaf46ccfa97741c8ded64e7094ad98058
eabdullin <ed.abdullin.1@gmail.com>
2023-12-27 13:30:56 +0300

    Remove exit with status1

--- check_epel_synergy.py
+++ check_epel_synergy.py
@@ -95,8 +95,6 @@ def main():
             print(f"Package exists in both EPEL and Synergy for version {version}: {package}")
     if not interserctions_found:
         print("No packages found in both EPEL and Synergy repos")
-    else:
-        exit(1)
 
 if __name__ == '__main__':
     main()
